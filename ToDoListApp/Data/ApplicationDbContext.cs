598c0af37aaabd4e4db1eecac684bd3662cb8aa8
OmanDev <omar.chero.borja@gmail.com>
2023-10-01 21:04:17 -0500

    Creación Tabla TaskData

---
+++ ToDoListApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListApp.Entities;
+
+namespace ToDoListApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaskData> TaskDatas { get; set; }
+    }
+}
---
+++ ToDoListApp/Entities/TaskData.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApp.Entities
+{
+    public class TaskData
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(150, 
+                      MinimumLength = 6, 
+                      ErrorMessage = "El campo {0} requiere como mínimo {2} y máximo {1} caracteres")]
+        [Display(Name = "Título")]
+        public string Title { get; set; }
+
+        [StringLength(150,
+                      MinimumLength = 6,
+                      ErrorMessage = "El campo {0} requiere como mínimo {2} y máximo {1} caracteres")]
+        [Display(Name = "Descripción")]
+
+        public string Description { get; set; }
+
+        [Display(Name = "Orden")]
+        public int Order { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Creación")]
+        public DateTime CreationDate { get; set; }
+    }
+}
